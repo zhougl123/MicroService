2b7cf00f283a2883b7e06a6d89629247610277c9
zhougl <zhougl@sunline.com>
2019-01-15 16:54:29 +0800

    fifth commit

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -19,6 +19,7 @@ public class ConfigBean {
 
     @Bean
     public IRule myIRule(){
+        //12121212121
         //jkjkjkjkjj
         //测试git到githup远程库的提交//
         //return new RoundRobinRule();//重新配置负载均衡算法
